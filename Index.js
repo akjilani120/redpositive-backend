7ed064f062f7e2260570453414975034cd2eae1a
unknown <abdulkaderjilani120@gmail.com>
2022-08-16 01:21:13 +0600

    added get data

--- Index.js
+++ Index.js
@@ -10,11 +10,7 @@ app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dj06fwm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-  
-//   // perform actions on the collection object
-//   client.close();
-// });
+
  async function run () {
     try{
         await client.connect()
